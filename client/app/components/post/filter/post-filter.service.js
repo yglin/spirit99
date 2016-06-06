137052e3531a51fdc2bf603a0507955f0bb8ff7a
yglin <yglin.mlanser@gmail.com>
2016-06-06 11:58:11 +0800

    Post Filter: add event start and end

--- client/app/components/post/filter/post-filter.service.js
+++ client/app/components/post/filter/post-filter.service.js
@@ -5,15 +5,18 @@
         .module('spirit99')
         .service('PostFilter', PostFilter);
 
-    PostFilter.$inject = ['$rootScope', 'DatePeriod', 'Category'];
+    PostFilter.$inject = ['$rootScope', '$routeParams', 'DatePeriod', 'Category'];
 
     /* @ngInject */
-    function PostFilter($rootScope, DatePeriod, Category) {
+    function PostFilter($rootScope, $routeParams, DatePeriod, Category) {
         var self = this;
         self.keywords = [];
         self.createTime = {};
+        self.event = {};
+        self.isEvent = undefined;
         self.categories = Category.categories;
         self.filter = filter;
+        self.onChange = onChange;
         self.onAddKeyword = onAddKeyword;
         self.onRemoveKeyword = onRemoveKeyword;
         self.onChangeDatePeriod = onChangeDatePeriod;
@@ -25,6 +28,10 @@
             self.createTime.preset = 'anyTime';
             self.createTime.start = DatePeriod.getPresetStart(self.createTime.preset);
             self.createTime.end = DatePeriod.getPresetEnd(self.createTime.preset);
+        }
+
+        function onChange() {
+            $rootScope.$broadcast('post:filterChanged');
         }
 
         function onAddKeyword() {
@@ -44,8 +51,30 @@
             }
 
             // Filter by create_time in-between date period
-            if (!DatePeriod.inBetween(post.create_time, self.createTime.start, self.createTime.end)) {
+            if (self.createTime.start && self.createTime.end
+                && !DatePeriod.inBetween(post.create_time, self.createTime.start, self.createTime.end)) {
                 return false;
+            }
+
+            if (self.isEvent && self.event.start) {
+                if (!post.startAt) {
+                    return false;
+                }
+                var startAt = new Date(post.startAt);
+                if (startAt < self.event.start) {
+                    return false;
+                }
+            }
+
+            if (self.isEvent && self.event.end) {
+                if (!post.endAt) {
+                    return false;
+                }
+                var endAt = new Date(post.endAt);
+                var endMidnight = new Date(self.event.end.getTime() + 24 * 60 * 60 * 1000);
+                if (endAt > endMidnight) {
+                    return false;
+                }
             }
 
             if (!Category.isVisible(post.category)) {
