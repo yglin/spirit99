aac049d200d2ecce5b09f6ca01261b4ea8dcafb1
yglin <yglin.mlanser@gmail.com>
2016-06-16 15:00:38 +0800

    Map view - add initial loading map process indicator

--- client/app/components/map/map.controller.js
+++ client/app/components/map/map.controller.js
@@ -5,12 +5,14 @@
     .module('spirit99')
     .controller('MapController', MapController);
 
-    MapController.$inject = ['Map', 'CONFIG'];
+    MapController.$inject = ['$scope', 'Map'];
 
     /* @ngInject */
-    function MapController(Map) {
+    function MapController($scope, Map) {
         var mapVM = this;
         mapVM.title = 'MapController';
+        mapVM.mapIsReady = false;
+
         // Map events
         mapVM.events = {
             'idle': Map.broadcastEvent,
@@ -32,51 +34,11 @@
             Map.prmsGetInitMap().then(function () {
                 mapVM.map = Map.map;
             });
+
+            var stopListenMapIdle = $scope.$on('map:idle', function () {
+                mapVM.mapIsReady = true;
+                stopListenMapIdle();
+            })
         }
-
-        //////////////// Custom Event Handlers ///////////////////
-        // function handlerMarkersRefresh (eventName, markers) {
-        //     if(markers.length >= CONFIG.MIN_POSTS_FOR_LIST){
-        //         mapVM.showListButton = true;
-        //     }
-        //     else{
-        //         mapVM.showListButton = false;                
-        //     }
-        // };
-
-        // function handlerDragStart (mapObject, eventName) {
-        //     // options = typeof options === 'undefined' ? {} : options;
-        //     // options.optionArg = typeof options.optionArg === 'undefined' ? defaultValue : options.optionArg;
-        //     mapVM.isDragging = true;
-        // }
-
-        // function handlerDragEnd (mapObject, eventName) {
-        //     mapVM.isDragging = false;
-        //     broadcastMapEvent(mapObject, eventName);
-        // }
-
-        //////////////// Utility Functions ////////////////
-
-        // function locate(event) {
-        //     $mdDialog.show({
-        //         templateUrl: 'app/views/locater/locater.tpl.html',
-        //         controller: 'LocaterController',
-        //         controllerAs: 'locaterVM',
-        //         targetEvent: event,
-        //         clickOutsideToClose:true
-        //     });
-        // }
-
-        // function getInitialMap () {
-        //     return {
-        //         center: initMapArea.center,
-        //         zoom: initMapArea.zoom
-        //     };
-        // }
-
-        // function openPostList() {
-        //     // UserCtrls.selectedSidenav = 'post-list';
-        //     $mdSidenav('sidenav-main').open();
-        // }
     }
 })();
