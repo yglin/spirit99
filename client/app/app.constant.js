c253dfd5442489427d99c571a1fe751415d40cc5
yglin <yglin.mlanser@gmail.com>
2016-06-17 10:23:10 +0800

    Stop tracking app.constant.js for it is auto generated

--- client/app/app.constant.js
+++
@@ -1,9 +0,0 @@
-(function(angular, undefined) {
-'use strict';
-
-angular.module('spirit99')
-
-.constant('CONFIG', {MIN_POSTS_FOR_LIST:10,CHANNEL_SAVING_FIELDS:['portal-url','id','title','description','logo-url'],ENV:'production',DEBUG:false,MARKER_CLUSTER_TYPE:'cluster',GOOGLE_CLIENT_API_KEY:'AIzaSyB72lwL0HWu-jdurOAWFMIUMPAL6aHeZ0s'})
-
-;
-})(angular);
