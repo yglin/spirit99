0009e565bc693b1314a628acb70c1b95f04e3c89
yglin <yglin.mlanser@gmail.com>
2016-07-23 14:35:35 +0800

    Post View Dialog only allowed in $mdMedia("gt-sm")

--- client/app/components/post/viewer/post-viewer.service.js
+++ client/app/components/post/viewer/post-viewer.service.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-07-23 11:17:55
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-07-23 13:37:12
+* @Last Modified time: 2016-07-23 14:33:29
 */
 
 (function() {
@@ -12,10 +12,10 @@
         .module('spirit99')
         .service('PostViewer', PostViewer);
 
-    PostViewer.$inject = ['$rootScope', '$mdDialog'];
+    PostViewer.$inject = ['$rootScope', '$mdDialog', '$mdMedia'];
 
     /* @ngInject */
-    function PostViewer($rootScope, $mdDialog) {
+    function PostViewer($rootScope, $mdDialog, $mdMedia) {
         var self = this;
         self.showDialog = showDialog;
 
@@ -24,12 +24,17 @@
         activate();
 
         function activate() {
-            $rootScope.$on('post:show', function (event, post) {
-                self.showDialog(post);
-            });
+            if ($mdMedia('gt-sm')) {
+                $rootScope.$on('post:show', function (event, post) {
+                    self.showDialog(post);
+                });                
+            }
         }
 
         function showDialog(post) {
+            if (!$mdMedia('gt-sm')) {
+                return;
+            }
             var template = '<md-dialog style="background-color: rgba(0,0,0,0.5);" aria-label="Post View Dialog">';
             template += '<s99-post-viewer post="$ctrl.post"></s99-post-viewer>';
             template += '</md-dialog>';
