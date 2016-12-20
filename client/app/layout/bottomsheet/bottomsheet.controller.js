7fb6711e9da314e7e1f1e8f93b5d5c01d6d4eea1
yglin <yglin.mlanser@gmail.com>
2016-12-20 16:45:44 +0800

    Show filtered posts in bottomsheet

--- client/app/layout/bottomsheet/bottomsheet.controller.js
+++ client/app/layout/bottomsheet/bottomsheet.controller.js
@@ -2,7 +2,7 @@
 * @Author: yglin
 * @Date:   2016-07-20 20:44:54
 * @Last Modified by:   yglin
-* @Last Modified time: 2016-07-23 11:56:23
+* @Last Modified time: 2016-12-20 16:42:01
 */
 
 (function() {
@@ -12,14 +12,14 @@
     .module('spirit99')
     .controller('BottomsheetController', BottomsheetController);
 
-    BottomsheetController.$inject = ['$rootScope', 'Post'];
+    BottomsheetController.$inject = ['$scope', '$rootScope', 'Post'];
 
     /* @ngInject */
-    function BottomsheetController($rootScope, Post) {
+    function BottomsheetController($scope, $rootScope, Post) {
         var $ctrl = this;
         $ctrl.title = 'Bottomsheet';
         $ctrl.isOpen = false;
-        $ctrl.posts = undefined;
+        $ctrl.posts = [];
 
         $ctrl.toggleOpen = toggleOpen;
         $ctrl.onClickPost = onClickPost;
@@ -29,7 +29,15 @@
         ////////////////
 
         function activate() {
-            $ctrl.posts = Post.posts;
+            $scope.$on('post:reload', function () {
+                $ctrl.posts.length = 0;
+                Post.addFilteredPosts($ctrl.posts);
+            });
+
+            $scope.$on('post:filterChanged', function () {
+                $ctrl.posts.length = 0;
+                Post.addFilteredPosts($ctrl.posts);
+            });
         }
 
         function toggleOpen() {
